7b0ce5c48b9016b4d6ffb2fdc8bd3ada08ea9443
Pei Qing 卿培 <edwardtoday@gmail.com>
2014-12-25 16:32:56 +0800

    hypnosister

---
+++ Hypnosister/Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by edwardtoday on 12/25/14.
+//  Copyright (c) 2014 edwardtoday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
