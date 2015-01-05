ae91db08afe645549f072528eb8fe990f0a86bb3
Pei Qing 卿培 <edwardtoday@gmail.com>
2015-01-05 16:31:11 +0800

    Homepwner

---
+++ Homepwner/Homepwner/BNRItemsViewController.m
@@ -0,0 +1,52 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by qingpei on 1/5/15.
+//  Copyright (c) 2015 qingpei. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init {
+  // Call the superclass's designated initializer
+  self = [super initWithStyle:UITableViewStylePlain];
+  if (self) {
+    for (int i = 0; i < 5; i++) {
+      [[BNRItemStore sharedStore] createItem];
+    }
+  }
+  return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+  return [self init];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  // Create an instance of UITableViewCell, with default appearance
+  UITableViewCell *cell =
+      [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                             reuseIdentifier:@"UITableViewCell"];
+
+  // Set the text on the cell with the description of the item that is at the
+  // nth index of items, where n = row this cell will appear in on the tableview
+  NSArray *items = [[BNRItemStore sharedStore] allItems];
+  BNRItem *item = items[indexPath.row];
+
+  cell.textLabel.text = [item description];
+
+  return cell;
+}
+
+@end
